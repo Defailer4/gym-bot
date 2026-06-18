fa175bf720fc0d45cc0a63a2faafc8053c2f273b
ZombieVanya <Itrochin19@gmail.com>
2026-06-18 19:45:07 +0300

    add database in bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,27 @@
 from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
-from handlers.start import router as start_router
 import asyncio
 import os
+import logging
+
+from handlers.start import router as start_router
+from database.db import init_db
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def main():
+    logging.basicConfig(level=logging.INFO)
+
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher(bot=bot)
 
     dp.include_router(start_router)
+
+    logging.info("Инициализация базы данных...")
+    await init_db()
+    logging.info("База данных успешно инициализирована")
 
     await dp.start_polling(bot)
 
