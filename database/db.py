63d4ef2c68edeee02807bb8cd21f3b6bee0b672d
ZombieVanya <Itrochin19@gmail.com>
2026-06-18 19:40:13 +0300

    add database structure

--- database/db.py
+++ database/db.py
@@ -1,1 +1,69 @@
-#later
+import aiosqlite
+
+from database.base_exercises import BASE_EXERCISES
+
+DB_NAME = "gym_bot.db"
+
+async def init_db():
+    async with aiosqlite.connect(DB_NAME) as db:
+
+        await db.execute("PRAGMA foreign_keys = ON;")
+
+        await db.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+                user_id INTEGER PRIMARY KEY,
+                joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        """)
+
+        await db.execute("""
+            CREATE TABLE IF NOT EXISTS exercises (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL UNIQUE,
+                category TEXT NOT NULL,
+                user_id INTEGER NULL, 
+                FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
+            )
+        """)
+
+        await db.execute("""
+            CREATE TABLE IF NOT EXISTS workouts (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER NOT NULL,
+                start_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                end_time TIMESTAMP NULL,
+                FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
+            )
+        """)
+
+        await db.execute("""
+            CREATE TABLE IF NOT EXISTS workout_sets (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                workout_id INTEGER NOT NULL,
+                exercise_id INTEGER NOT NULL,
+                set_number INTEGER NOT NULL,
+                weight REAL NOT NULL,
+                reps INTEGER NOT NULL,
+                FOREIGN KEY (workout_id) REFERENCES workouts (id) ON DELETE CASCADE,
+                FOREIGN KEY (exercise_id) REFERENCES exercises (id) ON DELETE CASCADE
+            )
+        """)
+
+        await db.execute("""
+            CREATE TABLE IF NOT EXISTS water_logs (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER NOT NULL,
+                amount_ml = INTEGER NOT NULL,
+                timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
+            )
+        """)
+
+        prepared_exercises = [(name, category, None) for name, category in BASE_EXERCISES]
+
+        await db.executemany("""
+            INSERT OR IGNORE INTO exercises (name, category, user_id) 
+            VALUES (?, ?, ?)
+        """, prepared_exercises)
+
+        await db.commit()
